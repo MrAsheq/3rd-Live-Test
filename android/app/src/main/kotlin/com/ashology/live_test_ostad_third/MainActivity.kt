8b30e293daad15bbdb1cbd97166122cc6bc54950
mrAsh <misterasheq@outlook.com>
2025-09-01 21:50:08 +0600

    Ostad- 3rd Live Test

---
+++ android/app/src/main/kotlin/com/ashology/live_test_ostad_third/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ashology.live_test_ostad_third
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("3rd Live Test"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Text("Start from here")
+        ],
+      ),
+    );
+  }
+}
